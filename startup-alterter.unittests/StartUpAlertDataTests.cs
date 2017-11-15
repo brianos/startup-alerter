c0b2767caf81d27aedbe8eeda474ba9c8ebf3b5c
Brian O'Sullivan <briangos@gmail.com>
2017-11-15 14:06:52 +0000

    unit tests refactored and additional test cases added

--- startup-alterter.unittests/StartUpAlertDataTests.cs
+++ startup-alterter.unittests/StartUpAlertDataTests.cs
@@ -20,12 +20,40 @@ namespace startup_alterter.unittests
                 () => new StartUpAlertData(accountSid, authToken, fromNumber, toNumber, body));
         }
 
+        [TestCase("  ", "authToken", "fromNumber", "toNumber", "body")]
+        [TestCase("accountSid", "  ", "fromNumber", "toNumber", "body")]
+        [TestCase("accountSid", "authToken", "  ", "toNumber", "body")]
+        [TestCase("accountSid", "authToken", "fromNumber", "  ", "body")]
+        [TestCase("accountSid", "authToken", "fromNumber", "toNumber", "  ")]
+        public void ValidationShouldPreventWhitespaceInput(string accountSid, string authToken,
+                                                        string fromNumber, string toNumber,
+                                                        string body)
+        {
+            Assert.Throws<ArgumentException>(
+                () => new StartUpAlertData(accountSid, authToken, fromNumber, toNumber, body));
+        }
+
+        [TestCase(null, "authToken", "fromNumber", "toNumber", "body")]
+        [TestCase("accountSid", null, "fromNumber", "toNumber", "body")]
+        [TestCase("accountSid", "authToken", null, "toNumber", "body")]
+        [TestCase("accountSid", "authToken", "fromNumber", null, "body")]
+        [TestCase("accountSid", "authToken", "fromNumber", "toNumber", null)]
+        public void ValidationShouldPreventNullInput(string accountSid, string authToken,
+                                                    string fromNumber, string toNumber,
+                                                    string body)
+        {
+            Assert.Throws<ArgumentException>(
+                () => new StartUpAlertData(accountSid, authToken, fromNumber, toNumber, body));
+        }
+
         [Test]
         public void ConstructorShouldSetTheAccountSid()
         {
-            var startUpAlertData = new StartUpAlertData("accountSid", "authToken",
-                                                        "fromNumber", "toNumber",
-                                                        "body");
+            var startUpAlertData = new StartUpAlertData(accountSid: "accountSid",
+                                                        authToken: "authToken",
+                                                        fromNumber: "fromNumber",
+                                                        toNumber: "toNumber",
+                                                        body: "body");
 
             Assert.AreEqual("accountSid", startUpAlertData.AccountSid);
         }
@@ -33,9 +61,11 @@ namespace startup_alterter.unittests
         [Test]
         public void ConstructorShouldSetTheAuthToken()
         {
-            var startUpAlertData = new StartUpAlertData("accountSid", "authToken",
-                                                        "fromNumber", "toNumber",
-                                                        "body");
+            var startUpAlertData = new StartUpAlertData(accountSid: "accountSid",
+                                                        authToken: "authToken",
+                                                        fromNumber: "fromNumber",
+                                                        toNumber: "toNumber",
+                                                        body: "body");
 
             Assert.AreEqual("authToken", startUpAlertData.AuthToken);
         }
@@ -43,9 +73,11 @@ namespace startup_alterter.unittests
         [Test]
         public void ConstructorShouldSetTheFromNumber()
         {
-            var startUpAlertData = new StartUpAlertData("accountSid", "authToken",
-                                                        "fromNumber", "toNumber",
-                                                        "body");
+            var startUpAlertData = new StartUpAlertData(accountSid: "accountSid",
+                                                        authToken: "authToken",
+                                                        fromNumber: "fromNumber",
+                                                        toNumber: "toNumber",
+                                                        body: "body");
 
             Assert.AreEqual("fromNumber", startUpAlertData.FromNumber);
         }
@@ -53,9 +85,11 @@ namespace startup_alterter.unittests
         [Test]
         public void ConstructorShouldSetTheToNumber()
         {
-            var startUpAlertData = new StartUpAlertData("accountSid", "authToken",
-                                                        "fromNumber", "toNumber",
-                                                        "body");
+            var startUpAlertData = new StartUpAlertData(accountSid: "accountSid",
+                                                        authToken: "authToken",
+                                                        fromNumber: "fromNumber",
+                                                        toNumber: "toNumber",
+                                                        body: "body");
 
             Assert.AreEqual("toNumber", startUpAlertData.ToNumber);
         }
@@ -63,9 +97,11 @@ namespace startup_alterter.unittests
         [Test]
         public void ConstructorShouldSetTheBody()
         {
-            var startUpAlertData = new StartUpAlertData("accountSid", "authToken",
-                                                        "fromNumber", "toNumber",
-                                                        "body");
+            var startUpAlertData = new StartUpAlertData(accountSid: "accountSid",
+                                                        authToken: "authToken",
+                                                        fromNumber: "fromNumber",
+                                                        toNumber: "toNumber",
+                                                        body: "body");
 
             Assert.AreEqual("body", startUpAlertData.Body);
         }
